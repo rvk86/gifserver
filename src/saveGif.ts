2ea45957d7918bc18998dad3bb428a277ae967f0
ralph <ralph.vankrimpen@gmail.com>
2023-04-14 22:24:25 +0200

    update saveGif

--- src/saveGif.ts
+++ src/saveGif.ts
@@ -1,11 +1,9 @@
-import { writeFileSync } from "fs";
 import { getGif } from "./utils";
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 console.log(url);
 console.log(filePath);
-getGif(url).then((gif) => {
+getGif(url, filePath).then((gif) => {
   console.log("SUCCESS");
-  writeFileSync(filePath, gif);
 });
